297582c33037d162ae54d8718457e17f86374141
mikkame <mikami@tecpresso.co.jp>
2018-11-05 16:50:50 +0900

    setup database

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,18 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $faker = Faker\Factory::create('ja_JP');
+
+        foreach (range(0,100) as $i) {
+            $box = new App\Box;
+            $box->name = 'Box-'.$faker->date($format='Y-m-d');
+            $box->save();
+            foreach (range(0,mt_rand(1,10)) as $j) {
+                $item = new App\Item;
+                $item->box_id = $box->id;
+                $item->name = 'Item-'.$faker->phoneNumber;
+                $item->save();
+            }
+        }
     }
 }
